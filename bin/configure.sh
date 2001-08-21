e4bca5936fe516f74c7b0cc60cbdb7c772633524
Steffen A. Mork <sm@morknet.de>
2001-08-21 14:21:27 +0000

    - New selecting code
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@73 c0f852eb-cc16-0410-a65c-258990c0cc53

--- bin/configure.sh
+++ bin/configure.sh
@@ -7,6 +7,7 @@ make -C src clean
 
 chgrp -R blz3 *
 rm -f lib/*/lib*.a
+rm -f lib/*/*.lib
 rm -f bin/*/*
 
 chmod 770 bin/*
@@ -16,7 +17,11 @@ find include* lib WWW     doc man -type f -exec chmod 664 {} \;
 find src                          -type d -exec chmod 770 {} \;
 find src -name "*.c*"                     -exec chmod 660 {} \;
 
-find . -name "*.log" -exec rm {} \;
-
-cd src
-
+find $BLZ3_HOME -name "*.log" -exec rm {} \;
+find $BLZ3_HOME -name "*.ncb" -exec rm {} \;
+find $BLZ3_HOME -name "*.plg" -exec rm {} \;
+find $BLZ3_HOME -name "*.mak" -exec rm {} \;
+find $BLZ3_HOME -name "*.bak" -exec rm {} \;
+find $BLZ3_HOME -name "*~"    -exec rm {} \;
+find $BLZ3_HOME -name ".#*"   -exec rm {} \;
+rm -rf $BLZ3_HOME/Debug $BLZ3_HOME/Release
--- bin/setup.sh
+++ bin/setup.sh
@@ -11,10 +11,15 @@ setenv BLZ3_SYSINC   $BLZ3_HOME/include_unix
 setenv BLZ3_LIB      "$BLZ3_HOME/lib/$OSTYPE"_"$CPUTYPE"
 setenv BLZ3_BIN      "$BLZ3_HOME/bin/$OSTYPE"_"$CPUTYPE"
 
-setenv LIB_B3        "-lB3System -lB3Base -lB3Raytrace -lB3Image -lB3jpeg -lB3tiff"
+setenv LIB_B3        "-lB3Raytrace -lB3Image -lB3jpeg -lB3tiff -lB3Base -lB3System -lB3Base"
 setenv LIB_X3        "-lX11 -lXt"
 setenv LIB_S3        "-lm -lpthread"
-setenv LIB_OGL3      "-lGL -lglut -lGLU -lXmu"
+
+if ( $?BLZ3_USE_OPENGL ) then
+  setenv LIB_OGL3    "-lGL -lglut -lGLU -lXmu"
+else
+  setenv LIB_OGL3    ""
+endif
 
 setenv STDINC        "$ARCHFLAG -I$BLZ3_SYSINC -I$MOTIF_INCLUDE"
 setenv LDFLAGS       "-L$BLZ3_LIB -L$MOTIF_LIB"
