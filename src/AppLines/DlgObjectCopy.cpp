81c9a417c4c434ce4a52ae7ab1e4cd7aa564dd3d
Steffen A. Mork <sm@morknet.de>
2002-01-07 21:19:30 +0000

    - Adding object copy dialog
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@229 c0f852eb-cc16-0410-a65c-258990c0cc53

---
+++ src/AppLines/DlgObjectCopy.cpp
@@ -0,0 +1,108 @@
+/*
+**
+**	$Filename:	DlgObjectCopy.cpp $
+**	$Release:	Dortmund 2002 $
+**	$Revision$
+**	$Date$
+**	$Author$
+**	$Developer:	Steffen A. Mork $
+**
+**	Blizzard III - Dialog for copying objects
+**
+**	(C) Copyright 2001, 2002  Steffen A. Mork
+**	    All Rights Reserved
+**
+**
+*/
+
+/*************************************************************************
+**                                                                      **
+**                        Lines III includes                            **
+**                                                                      **
+*************************************************************************/
+
+#include "AppLines.h"
+#include "DlgObjectCopy.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/*************************************************************************
+**                                                                      **
+**                        Blizzard III development log                  **
+**                                                                      **
+*************************************************************************/
+
+/*
+**	$Log$
+**	Revision 1.1  2002/01/07 21:19:30  sm
+**	- Adding object copy dialog
+**
+**
+*/
+
+/*************************************************************************
+**                                                                      **
+**                        CDlgObjectCopy implementation                 **
+**                                                                      **
+*************************************************************************/
+
+CDlgObjectCopy::CDlgObjectCopy(CWnd* pParent /*=NULL*/)
+	: CDialog(CDlgObjectCopy::IDD, pParent)
+{
+	CWinApp *app = AfxGetApp();
+
+	//{{AFX_DATA_INIT(CDlgObjectCopy)
+	m_NumCopies = app->GetProfileInt(CB3ClientString(),"copy count", 1);
+	//}}AFX_DATA_INIT
+	m_OrigBBox = null;
+	m_Move.x   = atof(app->GetProfileString(CB3ClientString(),"move object x", "100.0"));
+	m_Move.y   = atof(app->GetProfileString(CB3ClientString(),"move object y",   "0.0"));
+	m_Move.z   = atof(app->GetProfileString(CB3ClientString(),"move object z",   "0.0"));
+	m_Rotate.x = atof(app->GetProfileString(CB3ClientString(),"rotate object x", "0.0"));
+	m_Rotate.y = atof(app->GetProfileString(CB3ClientString(),"rotate object y", "0.0"));
+	m_Rotate.z = atof(app->GetProfileString(CB3ClientString(),"rotate object z","90.0"));
+}
+
+
+void CDlgObjectCopy::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CDlgObjectCopy)
+	DDX_Control(pDX, IDC_ROT_Z, m_zCtrlRot);
+	DDX_Control(pDX, IDC_ROT_Y, m_yCtrlRot);
+	DDX_Control(pDX, IDC_ROT_X, m_xCtrlRot);
+	DDX_Control(pDX, IDC_OFFSET_Z, m_zCtrlOffset);
+	DDX_Control(pDX, IDC_OFFSET_Y, m_yCtrlOffset);
+	DDX_Control(pDX, IDC_OFFSET_X, m_xCtrlOffset);
+	DDX_Text(pDX, IDC_NUM_COPIES, m_NumCopies);
+	DDV_MinMaxUInt(pDX, m_NumCopies, 1, 1023);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgObjectCopy, CDialog)
+	//{{AFX_MSG_MAP(CDlgObjectCopy)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CDlgObjectCopy message handlers
+
+BOOL CDlgObjectCopy::OnInitDialog() 
+{
+	CDialog::OnInitDialog();
+	m_xCtrlOffset.b3SetValue(m_Move.x);
+	m_yCtrlOffset.b3SetValue(m_Move.y);
+	m_zCtrlOffset.b3SetValue(m_Move.z);
+	m_xCtrlRot.b3SetValue(m_Rotate.x);
+	m_yCtrlRot.b3SetValue(m_Rotate.y);
+	m_zCtrlRot.b3SetValue(m_Rotate.z);
+	// TODO: Add extra initialization here
+	
+	return TRUE;  // return TRUE unless you set the focus to a control
+	              // EXCEPTION: OCX Property Pages should return FALSE
+}
---
+++ src/AppLines/DlgObjectCopy.h
@@ -0,0 +1,75 @@
+/*
+**
+**	$Filename:	DlgObjectCopy.h $
+**	$Release:	Dortmund 2002 $
+**	$Revision$
+**	$Date$
+**	$Author$
+**	$Developer:	Steffen A. Mork $
+**
+**	Blizzard III - Dialog for copying objects
+**
+**	(C) Copyright 2002  Steffen A. Mork
+**	    All Rights Reserved
+**
+**
+*/
+
+#if !defined(AFX_DLGOBJECTCOPY_H__4DB0D317_038A_11D6_8A1B_00E018258F1A__INCLUDED_)
+#define AFX_DLGOBJECTCOPY_H__4DB0D317_038A_11D6_8A1B_00E018258F1A__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// DlgObjectCopy.h : header file
+//
+
+#include "blz3/raytrace/b3Raytrace.h"
+#include "blz3/system/b3FloatEdit.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CDlgObjectCopy dialog
+
+class CDlgObjectCopy : public CDialog
+{
+// Construction
+public:
+	b3_vector  m_Move;
+	b3_vector  m_Rotate;
+	b3BBox    *m_OrigBBox;
+	CDlgObjectCopy(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CDlgObjectCopy)
+	enum { IDD = IDD_OBJECT_COPY };
+	CB3FloatEdit	m_zCtrlRot;
+	CB3FloatEdit	m_yCtrlRot;
+	CB3FloatEdit	m_xCtrlRot;
+	CB3FloatEdit	m_zCtrlOffset;
+	CB3FloatEdit	m_yCtrlOffset;
+	CB3FloatEdit	m_xCtrlOffset;
+	b3_count	m_NumCopies;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CDlgObjectCopy)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CDlgObjectCopy)
+	virtual BOOL OnInitDialog();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_DLGOBJECTCOPY_H__4DB0D317_038A_11D6_8A1B_00E018258F1A__INCLUDED_)
