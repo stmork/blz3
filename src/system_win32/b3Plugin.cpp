22047614e8e7a7b395feb97fb531025bf05c91ba
Steffen A. Mork <sm@morknet.de>
2003-05-29 12:31:46 +0000

    - Added error messages
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@522 c0f852eb-cc16-0410-a65c-258990c0cc53

--- src/system_win32/b3Plugin.cpp
+++ src/system_win32/b3Plugin.cpp
@@ -32,9 +32,12 @@
 
 /*
 **	$Log$
+**	Revision 1.4  2003/05/29 12:31:46  sm
+**	- Added error messages
+**
 **	Revision 1.3  2003/05/24 14:10:45  sm
 **	- Updated platform independend support
-**
+**	
 **	Revision 1.2  2003/05/24 14:00:36  sm
 **	- Added plugin base classes for system independend support
 **	
@@ -72,7 +75,7 @@ b3PluginBase *b3Loader::b3CreatePlugin(b3Path &library)
 
 b3Plugin::b3Plugin(b3Path &library) : b3PluginBase(library)
 {
-	m_Handle = LoadLibrary(library);
+	m_Handle = AfxLoadLibrary(library);
 	if (m_Handle != null)
 	{
 		b3_plugin_init_func init;
@@ -85,6 +88,14 @@ b3Plugin::b3Plugin(b3Path &library) : b3PluginBase(library)
 			init();
 			b3PrintF(B3LOG_DEBUG,"Plugin %s initialized.\n",(const char *)m_PluginPath);
 		}
+		else
+		{
+			b3PrintF(B3LOG_NORMAL,"Init method for plugin %s failed to load!\n",(const char *)m_PluginPath);
+		}
+	}
+	else
+	{
+		b3PrintF(B3LOG_NORMAL,"Plugin %s failed to load!\n",(const char *)m_PluginPath);
 	}
 }
 
@@ -92,7 +103,7 @@ b3Plugin::~b3Plugin()
 {
 	if (m_Handle != null)
 	{
-		FreeLibrary(m_Handle);
+		AfxFreeLibrary(m_Handle);
 		b3PrintF(B3LOG_DEBUG,"Plugin %s unloaded.\n",(const char *)m_PluginPath);
 	}
 }
