65f1f427dd32dff55eba36780519f65aacc13766
Steffen A. Mork <sm@morknet.de>
2003-05-30 14:45:07 +0000

    - Added support for windows plugins.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@525 c0f852eb-cc16-0410-a65c-258990c0cc53

---
+++ include_unix/blz3/b3PluginDef.h
@@ -0,0 +1,25 @@
+/*
+**
+**	$Filename:      b3PluginDef.h $
+**	$Release:       Dortmund 2003 $
+**	$Revision$
+**	$Date$
+**	$Author$
+**	$Developer:     Steffen A. Mork $
+**
+**	Blizzard III - class import/export to plugins
+**
+**	(C) Copyright  Steffen A. Mork
+**	    All Rights Reserved
+**
+**
+**
+**
+*/
+
+#ifndef B3_PLUGINDEF_H
+#define B3_PLUGINDEF_H
+
+#define B3_PLUGIN
+
+#endif
