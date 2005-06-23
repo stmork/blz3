85a70b6c7b967113eeba12c082c6bbac7e34f02c
Steffen A. Mork <sm@morknet.de>
2005-06-23 16:27:01 +0000

    - Some changes made
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@1182 c0f852eb-cc16-0410-a65c-258990c0cc53

--- bin/WWW-setup.sh
+++ bin/WWW-setup.sh
@@ -2,12 +2,12 @@
 
 umask 002
 
+cd ~blz3/WWW/lines/small
+cp ../normal/dlg_*jpg ../normal/window_*jpg  .
+find . -name "dlg_*.jpg" -exec bconv3 -i {} -f -s 0.4 -j {} \;
+find . -name "window_*.jpg" -exec bconv3 -f -s 0.2 -i {} -j {} \;
+
 cd ~blz3
 chgrp -R blz3 WWW
 find WWW -type d | xargs chmod 775
 find WWW -type f |xargs chmod 664
-
-cd WWW/lines/small
-cp ../normal/dlg_*jpg ../normal/window_*jpg  .
-find . -name "dlg_*.jpg" -exec bconv3 -i {} -f -s 0.4 -j {} \;
-find . -name "window_*.jpg" -exec bconv3 -f -s 0.2 -i {} -j {} \;
