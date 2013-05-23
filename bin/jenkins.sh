72c6c19962db1ad2b2f2934058c6c05b14c40e5f
Steffen A. Mork <linux-dev@morknet.de>
2013-05-23 11:34:07 +0000

    * Build korrigiert.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@1554 c0f852eb-cc16-0410-a65c-258990c0cc53

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -11,29 +11,30 @@ umask 022
 mkdir -p ${BLZ3_DEBIAN}/usr/bin
 mkdir -p ${BLZ3_DEBIAN}/DEBIAN
 
-mkdir -p ${BLZ3_DEBIAN_DEV}/usr/share/blz3
+mkdir -p ${BLZ3_DEBIAN_DEV}/usr/share/doc/blz3
 mkdir -p ${BLZ3_DEBIAN_DEV}/usr/include
 mkdir -p ${BLZ3_DEBIAN_DEV}/usr/lib
 mkdir -p ${BLZ3_DEBIAN_DEV}/DEBIAN
 
-export FLAGS=-O3 -ffast-math -ftree-vectorize -s
-export CFLAGS="${FLAGS}"
-export CPPFLAGS="${FLAGS}"
-export CXXFLAGS="${FLAGS}"
+export CXXFLAGS="-O3 -ffast-math -ftree-vectorize -s"
+CFLAGS="${CXXFLAGS}"
+unset CPPFLAGS
 
 set -e
 
 make configure
-./configure BLZ3_DOC=${BLZ3_DEBIAN_DEV}/usr/share/blz3 --prefix=${BLZ3_DEBIAN} --exec-prefix=${BLZ3_DEBIAN}/usr
-make clean depend
+./configure BLZ3_DOC=${BLZ3_DEBIAN_DEV}/usr/share/doc/blz3 --prefix=${BLZ3_DEBIAN} --exec-prefix=${BLZ3_DEBIAN}/usr
+make depend
 make 
 make install documentation
+make test
 
 sed -e s/%ARCH%/${ARCH}/g control-blz3 >${BLZ3_DEBIAN}/DEBIAN/control
 dpkg -b ${BLZ3_DEBIAN} blz3.deb
 
 sed -e s/%ARCH%/${ARCH}/g control-blz3-dev >${BLZ3_DEBIAN_DEV}/DEBIAN/control
-cp -a include/blz3 include_unix/blz3 ${BLZ3_DEBIAN_DEV}/usr/include
+rsync -av include/blz3/      ${BLZ3_DEBIAN_DEV}/usr/include/blz3/
+rsync -av include_unix/blz3/ ${BLZ3_DEBIAN_DEV}/usr/include/blz3/
 cp -a lib/lib*.a ${BLZ3_DEBIAN_DEV}/usr/lib
 find  ${BLZ3_DEBIAN_DEV} -name .svn -type d | xargs rm -rf 
 dpkg -b ${BLZ3_DEBIAN_DEV} blz3-dev.deb
