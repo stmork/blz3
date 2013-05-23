753aecd0f946ca067232091742e6297ccb456fee
Steffen A. Mork <linux-dev@morknet.de>
2013-05-23 10:22:24 +0000

    * Jenkins baut Debian Packages.
    git-svn-id: svn+ssh://svn.morknet.de/usr/local/subversion/blz3/trunk@1550 c0f852eb-cc16-0410-a65c-258990c0cc53

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -2,11 +2,34 @@
 
 export BLZ3_HOME=$PWD
 export BLZ3_DOC=$BLZ3_HOME/doc
-#env
+
+BLZ3_DEBIAN=../debian/blz3
+BLZ3_DEBIAN_DEV=../debian/blz3-dev
+
+rm -rf ${BLZ3_DEBIAN} ${BLZ3_DEBIAN_DEV} 
+umask 022
+
+mkdir -p ${BLZ3_DEBIAN}/usr/bin
+mkdir -p ${BLZ3_DEBIAN}/DEBIAN
+
+mkdir -p ${BLZ3_DEBIAN_DEV}/usr/share/blz3
+mkdir -p ${BLZ3_DEBIAN_DEV}/usr/include
+mkdir -p ${BLZ3_DEBIAN_DEV}/usr/lib
+mkdir -p ${BLZ3_DEBIAN_DEV}/DEBIAN
 
 set -e
 
 make configure
-./configure
+./configure BLZ3_DOC=${BLZ3_DEBIAN_DEV}/usr/share/blz3 --prefix=${BLZ3_DEBIAN} --exec-prefix=${BLZ3_DEBIAN}/usr
 make clean depend
 make 
+make install documentation
+
+cp -a control-blz3 ${BLZ3_DEBIAN}/DEBIAN/control
+dpkg -b ${BLZ3_DEBIAN} blz3.deb
+
+cp -a control-blz3-dev ${BLZ3_DEBIAN_DEV}/DEBIAN/control
+cp -a include/blz3 include_unix/blz3 ${BLZ3_DEBIAN_DEV}/usr/include
+cp -a lib/lib*.a ${BLZ3_DEBIAN_DEV}/usr/lib
+find  ${BLZ3_DEBIAN_DEV} -name .svn -type d | xargs rm -rf 
+dpkg -b ${BLZ3_DEBIAN_DEV} blz3-dev.deb
