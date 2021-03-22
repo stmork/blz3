f9c2d98fdac69119ca2eff7aa138a3266a68385d
Steffen A. Mork <sm@morknet.de>
2021-03-22 14:38:26 +0100

    Prepared Jenkins build.

--- bin/jenkins-qt.sh
+++ bin/jenkins-qt.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-export BLZ3_HOME=$PWD
+set -e
 
+export BLZ3_HOME=$PWD
+export LD_LIBRARY_PATH=$BLZ3_HOME/lib
+
+export OFLAGS="-O2"
+export CFLAGS=""
+export CXXFLAGS=""
+export CPPFLAGS=""
+export JOBS=4
+
+test -e src/Makefile && make distclean
 make config depend
 
 cd src
-qmake CONFIG+=debug CONFIG+=lcov -r
+qmake CONFIG+=debug CONFIG+=gcov -r
 rm */Makefile
 cd ..
 
-make -j 4 install
+make -j ${JOBS}
+make install
+make cppcheck
 
+bin/brt3 -n -s 512 data/*
+cd src/unittest
+./unittest
+cd ../..
+
+make lcov
+make documentation
