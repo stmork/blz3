a1ec9b336746326ce878622870a73b60cf6aba65
Steffen A. Mork <linux-dev@morknet.de>
2021-04-02 19:33:58 +0200

    More algorithm tests.

--- src/unittest/b3SamplerTest.cc
+++ src/unittest/b3SamplerTest.cc
@@ -36,6 +36,7 @@
 #ifdef HAVE_LIBCPPUNIT
 
 CPPUNIT_TEST_SUITE_REGISTRATION(b3SamplerTest);
+#define SAMPLE_RESOLUTION   640
 
 /*************************************************************************
 **                                                                      **
@@ -132,6 +133,129 @@ public:
 	}
 };
 
+class b3WoodSampler : public b3ImageSampler, public b3Wood
+{
+	b3_coord m_Translate;
+	b3Color  m_Dark;
+	b3Color  m_Light;
+
+public:
+	b3WoodSampler(b3Tx * tx) : b3ImageSampler(tx)
+	{
+		b3_vector scale;
+		b3_f64    scalar = 200.0 / SAMPLE_RESOLUTION;
+
+		m_Translate = SAMPLE_RESOLUTION >> 1;
+		m_Dark      = B3_BLACK;
+		m_Light     = B3_WHITE;
+
+		b3InitWood();
+		b3Vector::b3Init(&scale, scalar, scalar, scalar);
+		b3PrepareWood(&scale);
+	}
+
+protected:
+	inline b3_color b3SamplePixel(const b3_coord x, const b3_coord y) override
+	{
+		b3_vector sample;
+		b3Color   result;
+
+		sample.y = x - m_Translate;
+		sample.x = m_Translate - y;
+		sample.z = m_Translate - x * 0.02;
+
+		b3_f64 mix = b3ComputeWood(&sample, 1.0);
+		result = b3Color::b3Mix(m_Dark, m_Light, mix);
+
+		return result;
+	}
+};
+
+class b3PlankSampler : public b3ImageSampler, public b3OakPlank
+{
+	b3_f64   m_Scale;
+	b3Color  m_Dark;
+	b3Color  m_Light;
+
+public:
+	b3PlankSampler(b3Tx * tx) : b3ImageSampler(tx)
+	{
+		b3_vector scale;
+		b3_f64    scalar = 10;
+
+		m_Scale     = 1.0 / SAMPLE_RESOLUTION;
+		m_Dark      = B3_BLACK;
+		m_Light     = B3_WHITE;
+
+		b3InitOakPlank();
+#ifdef CREATE_ICON
+		m_xOffset = 0.52;
+		m_RingFrequency *= 0.1;
+#endif
+		b3Vector::b3Init(&scale, scalar, scalar, scalar);
+		b3PrepareOakPlank(&scale);
+	}
+
+protected:
+	inline b3_color b3SamplePixel(const b3_coord x, const b3_coord y) override
+	{
+		b3_vector sample;
+		b3Color   result;
+		b3_index  index;
+
+		sample.x = x * m_Scale;
+		sample.y = y * m_Scale;
+		sample.z = 0;
+
+		b3_f64 mix = b3ComputeOakPlank(&sample, 1.0, index);
+		result = b3Color::b3Mix(m_Dark, m_Light, mix);
+
+		return result;
+	}
+};
+
+class b3WaterSampler : public b3ImageSampler, public b3Water
+{
+	b3Tx  * m_Tx;
+	b3_f64  m_Factor;
+	b3_f64  m_Time;
+
+public:
+	inline b3WaterSampler(b3Tx * tx) : b3ImageSampler(tx)
+	{
+		m_Factor = 1.0;
+		m_Time   = 0.0;
+
+		b3PrintF(B3LOG_NORMAL, "octaves=%ld\n", m_Octaves);
+		b3PrintF(B3LOG_NORMAL, "Km=%3.3f\n", m_Km);
+		b3PrintF(B3LOG_NORMAL, "wind: frequency=%3.3f min=%3.3f amp=%3.3f\n",
+			m_WindFreq, m_MinWind, m_WindAmp);
+	}
+
+	inline void b3SampleTime(b3_f64 time)
+	{
+		m_Time = time;
+		b3PrepareWater(m_Time);
+		b3Sample();
+	}
+
+private:
+	inline b3_color b3SamplePixel(const b3_coord x, const b3_coord y) override
+	{
+		b3_vector pos;
+		b3_f64    water;
+
+		pos.x = m_Factor * x / m_xMax;
+		pos.y = m_Factor * y / m_yMax;
+		pos.z = 0;
+
+		water = b3ComputeWater(&pos) * 0.5;
+
+		return b3Color(water, water, water);
+	}
+};
+
+
 /*************************************************************************
 **                                                                      **
 **                        Sampler tests                                 **
@@ -152,17 +276,23 @@ void b3SamplerTest::test()
 {
 	b3Tx tx;
 
-	CPPUNIT_ASSERT(tx.b3AllocTx(512, 512, 128));
+	CPPUNIT_ASSERT(tx.b3AllocTx(SAMPLE_RESOLUTION, SAMPLE_RESOLUTION, 128));
 
 	b3MatWoodSampler      sampler_mat_wood(&tx);
 	b3MatOakPlankSampler  sampler_mat_oak(&tx);
 	b3BumpWoodSampler     sampler_bump_wood(&tx);
 	b3BumpOakPlankSampler sampler_bump_oak(&tx);
+	b3WoodSampler         sampler_wood(&tx);
+	b3PlankSampler        sampler_plank(&tx);
+	b3WaterSampler        sampler_water(&tx);
 
 	sample(sampler_mat_wood,  tx, "img_test_sample_mw.tga");
 	sample(sampler_mat_oak,   tx, "img_test_sample_mo.tga");
 	sample(sampler_bump_wood, tx, "img_test_sample_bw.tga");
 	sample(sampler_bump_oak,  tx, "img_test_sample_bo.tga");
+	sample(sampler_wood,      tx, "img_test_sample_wood.tga");
+	sample(sampler_plank,     tx, "img_test_sample_plank.tga");
+	sample(sampler_water,     tx, "img_test_sample_water.tga");
 }
 
 void b3SamplerTest::sample(b3Sampler & sampler, b3Tx & tx, const char * image_name)
