9e7dd0cff85ff9f38502053f3f2d5b3a34ed2304
Steffen A. Mork <linux-dev@morknet.de>
2021-04-02 13:48:56 +0200

    Corrected BMP parsing.

--- src/image/b3TxEasy.cc
+++ src/image/b3TxEasy.cc
@@ -26,6 +26,121 @@
 #include "b3TxInclude.h"
 #include <blz3/base/b3Endian.h>
 
+/**
+ * BMP format header.
+ *
+ * Source: https://de.wikipedia.org/wiki/Windows_Bitmap
+ */
+#pragma pack(2)
+struct b3HeaderBMP
+{
+	uint16_t bfType;     //!< ASCII-Zeichenkette "BM" (Hex: 0x42 0x4D, Dezimal: 66 77).
+	uint32_t bfSize;     //!< Größe der BMP-Datei in Byte. (unzuverlässig)
+	uint32_t bfReserved; //!< Reserviert, von der Software abhängig, standardmäßig 0
+
+	/**
+	 * Offset der Bilddaten in Byte vom Beginn der Datei an.
+	 *
+	 *  ieser hat nicht immer den Wert 54 (er ergibt sich aus 14 Byte Header +
+	 *  40 Byte Infoblock) und muss daher dynamisch ausgelesen werden, weil es
+	 *  sonst ggf. zu Fehldarstellungen im Bild kommt.
+	 */
+	uint32_t bfOffBits;
+
+	/** Größe der BITMAPINFOHEADER-Struktur in Byte. */
+	uint32_t biSize;
+
+	/**
+	 * Breite der Bitmap in Pixel. Dabei ist das erste Byte niederwertig und
+	 * das letzte Byte höchstwertig.
+	 */
+	int32_t biWidth;
+
+	/**
+	 * Der Betrag gibt die Höhe der Bitmap in Pixel an. Dabei ist das erste
+	 * Byte niederwertig und das letzte Byte höchstwertig.
+	 *
+	 * <ul>
+	 * <li>Ist der Wert positiv, so ist die Bitmap eine sogenannte
+	 * "bottom-up"-Bitmap (die Bilddaten beginnen mit der untersten und enden
+	 * mit der obersten Bildzeile). Dies ist die gebräuchlichste Variante.</li>
+	 *
+	 * <li>Ist der Wert negativ, so ist die Bitmap eine “top-down”-Bitmap (die
+	 * Bilddaten beginnen mit der obersten und enden mit der untersten Bildzeile).</li>
+	 * </ul>
+	 */
+	int32_t biHeight;
+
+	/**
+	 * 1 (Stand in einigen älteren Formaten wie PCX für die Anzahl der
+	 * Farbebenen, wird aber für BMP nicht verwendet)
+	 */
+	uint16_t biPlanes;
+
+	/**
+	 * Gibt die Farbtiefe der Bitmap in bpp an; muss einer der folgenden
+	 * Werte sein: 1, 4, 8, 16, 24 oder 32. Bei 1, 4 und 8 bpp sind die Farben
+	 * indiziert.
+	 */
+	uint16_t biBitCount;
+
+	/**
+	 * Einer der folgenden Werte:
+	 *
+	 * <ul>
+	 * <li>0 (BI_RGB): Bilddaten sind unkomprimiert.</li>
+	 * <li>1 (BI_RLE8): Bilddaten sind lauflängenkodiert für 8 bpp. Nur erlaubt
+	 * wenn biBitCount=8 und biHeight positiv.</li>
+	 * <li>2 (BI_RLE4): Bilddaten sind lauflängenkodiert für 4 bpp. Nur
+	 * erlaubt wenn biBitCount=4 und biHeight positiv.</li>
+	 * <li>3 (BI_BITFIELDS): Bilddaten sind unkomprimiert und benutzerdefiniert
+	 * (mittels Farbmasken) kodiert. Nur erlaubt wenn biBitCount=16 oder 32.</li>
+	 * </ul>
+	 */
+	uint32_t biCompression;
+
+	/**
+	 * Wenn biCompression=BI_RGB: Entweder 0 oder die Größe der Bilddaten in
+	 * Byte. Ansonsten: Größe der Bilddaten in Byte.
+	 */
+	uint32_t biSizeImage;
+
+	/**
+	 * Horizontale Auflösung des Zielausgabegerätes in Pixel pro Meter; wird
+	 * aber für BMP-Dateien meistens auf 0 gesetzt.
+	 */
+	int32_t biXPelsPerMeter;
+
+	/**
+	 * Vertikale Auflösung des Zielausgabegerätes in Pixel pro Meter; wird
+	 * aber für BMP-Dateien meistens auf 0 gesetzt.
+	 */
+	int32_t biYPelsPerMeter;
+
+	/**
+	 * Wenn biBitCount=1: 0. Wenn biBitCount=4 oder 8: die Anzahl der Einträge
+	 * der Farbtabelle; 0 bedeutet die maximale Anzahl (2, 16 oder 256).
+	 *
+	 * Ansonsten: Die Anzahl der Einträge der Farbtabelle (0=keine Farbtabelle).
+	 * Auch wenn sie in diesem Fall nicht notwendig ist, kann dennoch eine für
+	 * die Farbquantisierung empfohlene Farbtabelle angegeben werden.
+	 */
+	uint32_t biClrUsed;
+
+	/**
+	 * Wenn biBitCount=1, 4 oder 8: Die Anzahl sämtlicher im Bild verwendeten
+	 * Farben; 0 bedeutet alle Farben der Farbtabelle.
+	 *
+	 * Ansonsten:
+	 * Wenn eine Farbtabelle vorhanden ist und diese sämtliche im Bild
+	 * verwendeten Farben enthält: deren Anzahl.
+	 *
+	 * Ansonsten: 0.@brief biClrImportant
+	 */
+	uint32_t biClrImportant;
+};
+#pragma pack()
+
 /*************************************************************************
 **                                                                      **
 **                       Rayshade MTV-Format                            **
@@ -122,6 +237,7 @@ b3_result b3Tx::b3ParseRAW(
 
 b3_result b3Tx::b3ParseBMP(b3_u08 * buffer)
 {
+	b3HeaderBMP *  bmp = (b3HeaderBMP *)buffer;
 	b3_pkd_color * lPtr;
 	b3_u08    *    cPtr;
 	b3_res         xNewSize, yNewSize;
@@ -129,22 +245,34 @@ b3_result b3Tx::b3ParseBMP(b3_u08 * buffer)
 	b3_count       numPlanes, numColors;
 	b3_count       i, offset, value;
 
+	B3_ASSERT(offsetof(b3HeaderBMP, biSize) == 14);
+	B3_ASSERT(sizeof(b3HeaderBMP) == 54);
+	b3Endian::b3GetIntel32(&bmp->bfOffBits);
+	b3Endian::b3GetIntel32(&bmp->biSize);
+	xNewSize  = b3Endian::b3GetIntel32(&bmp->biWidth);
+	yNewSize  = b3Endian::b3GetIntel32(&bmp->biHeight);
+	b3Endian::b3GetIntel16(&bmp->biPlanes);
+	numPlanes = b3Endian::b3GetIntel16(&bmp->biBitCount);
+	b3Endian::b3GetIntel32(&bmp->biCompression);
+	b3Endian::b3GetIntel32(&bmp->biSizeImage);
+	b3Endian::b3GetIntel32(&bmp->biXPelsPerMeter);
+	b3Endian::b3GetIntel32(&bmp->biYPelsPerMeter);
+	numColors = b3Endian::b3GetIntel32(&bmp->biClrUsed);
+	b3Endian::b3GetIntel32(&bmp->biClrImportant);
+
 	b3PrintF(B3LOG_FULL, "IMG BMP  # b3ParseBMP(%s)\n",
 		(const char *)image_name);
 
-	if (b3Endian::b3GetIntel32(&buffer[30]) != 0)
+	if (bmp->biCompression != 0)
 	{
 		b3FreeTx();
 		b3PrintF(B3LOG_NORMAL, "IMG BMP  # Unsupported packing:\n");
 		B3_THROW(b3TxException, B3_TX_ERR_PACKING);
 	}
-	xNewSize  = b3Endian::b3GetIntel16(&buffer[18]);
-	yNewSize  = b3Endian::b3GetIntel16(&buffer[22]);
-	numColors = b3Endian::b3GetIntel32(&buffer[46]);
-	numPlanes = b3Endian::b3GetIntel32(&buffer[28]);
+
 	if (numColors == 0)
 	{
-		numColors = (b3Endian::b3GetIntel32(&buffer[10]) - 54) >> 2;
+		numColors = (bmp->bfOffBits - sizeof(b3HeaderBMP)) >> 2;
 	}
 
 	if (!b3AllocTx(xNewSize, yNewSize, numPlanes))
@@ -159,7 +287,7 @@ b3_result b3Tx::b3ParseBMP(b3_u08 * buffer)
 	{
 		if (numColors > 0)
 		{
-			b3_u08 * color_ptr = &buffer[54];
+			b3_u08 * color_ptr = &buffer[bmp->bfOffBits - numColors * 4];
 
 			for (i = 0; i < numColors; i++)
 			{
@@ -185,7 +313,7 @@ b3_result b3Tx::b3ParseBMP(b3_u08 * buffer)
 
 
 	/* checking bits per pixel */
-	buffer += b3Endian::b3GetIntel32(&buffer[10]);
+	buffer += bmp->bfOffBits;
 	switch (numPlanes)
 	{
 	case  1 :
@@ -260,7 +388,8 @@ b3_result b3Tx::b3ParseBMP(b3_u08 * buffer)
 		break;
 
 	case  8 :
-		cPtr = data + dSize;
+		cPtr  = data;
+		cPtr += dSize;
 		for (y = 0; y < ySize; y++)
 		{
 			cPtr   -= xSize;
