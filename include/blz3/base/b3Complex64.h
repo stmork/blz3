86daa489d52e0d091d69d6baf65ade6371e820ac
Steffen A. Mork <linux-dev@morknet.de>
2021-04-10 16:11:23 +0200

    Removed non SSE2 parts.

--- include/blz3/base/b3Complex64.h
+++ include/blz3/base/b3Complex64.h
@@ -29,40 +29,24 @@
 
 class B3_PLUGIN alignas(16) b3Complex64
 {
-#ifdef BLZ3_USE_SSE2
 	__m128d     v;
-#else
-	b3_f64      v[2];
-#endif
 
-#ifdef BLZ3_USE_SSE2
 	inline b3Complex64(const __m128d in)
 	{
-		SSE_PD_STORE(v, in);
+		v = in;
 	}
-#endif
 
 public:
 	inline b3Complex64() = default;
 
 	inline b3Complex64(const b3_f64 re)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = _mm_set_sd(re);
-#else
-		v[0] = re;
-		v[1] = 0;
-#endif
 	}
 
 	inline void operator=(const b3_f64 re)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = _mm_set_sd(re);
-#else
-		v[0] = re;
-		v[1] = 0;
-#endif
 	}
 
 	inline b3Complex64(const b3_f64 re, const b3_f64 im)
@@ -73,41 +57,17 @@ public:
 
 	inline b3Complex64(const b3Complex64 & orig)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = orig.v;
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			v[i] = orig.v[i];
-		}
-#endif
 	}
 
 	inline void operator=(const b3Complex64 & orig)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = orig.v;
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			v[i] = orig.v[i];
-		}
-#endif
 	}
 
 	inline static void b3CopyUncached(b3Complex64 & dst, const b3Complex64 & src)
 	{
-#ifdef BLZ3_USE_SSE2
 		_mm_stream_pd(&dst.b3Real(), src.v);
-#else
-		const int * srcPtr = reinterpret_cast<const int *>(&src.v);
-		int * dstPtr = reinterpret_cast<int *>(&dst.v);
-
-		_mm_stream_si32(dstPtr++, *srcPtr++);
-		_mm_stream_si32(dstPtr++, *srcPtr++);
-		_mm_stream_si32(dstPtr++, *srcPtr++);
-		_mm_stream_si32(dstPtr++, *srcPtr++);
-#endif
 	}
 
 	inline void operator=(b3Complex<b3_f64> & a)
@@ -116,68 +76,37 @@ public:
 		b3Imag() = a.b3Imag();
 	}
 
-	inline bool operator==(const b3Complex64 & a)
+	inline bool operator==(const b3Complex64 & a) const
 	{
-#ifdef BLZ3_USE_SSE2
 		__m128d eq = _mm_cmpneq_pd(v, a.v);
 
 		return _mm_movemask_pd(eq) == 0;
-#else
-#endif
 	}
 
 	inline b3Complex64 operator+(const b3Complex64 & sum) const
 	{
 		b3Complex64 result;
 
-#ifdef BLZ3_USE_SSE2
 		result.v = _mm_add_pd(v, sum.v);
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			result.v[i] = v[i] + sum.v[i];
-		}
-#endif
 		return result;
 	}
 
 	inline void operator+=(const b3Complex64 & sum)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = _mm_add_pd(v, sum.v);
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			v[i] += sum.v[i];
-		}
-#endif
 	}
 
 	inline b3Complex64 operator-(const b3Complex64 & sum) const
 	{
 		b3Complex64 result;
 
-#ifdef BLZ3_USE_SSE2
 		result.v = _mm_sub_pd(v, sum.v);
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			result.v[i] = v[i] - sum.v[i];
-		}
-#endif
 		return result;
 	}
 
 	inline void operator-=(const b3Complex64 & sum)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = _mm_sub_pd(v, sum.v);
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			v[i] -= sum.v[i];
-		}
-#endif
 	}
 
 	inline __m128d product(const b3Complex64 & mul) const
@@ -215,67 +144,35 @@ public:
 	{
 		b3Complex64 result;
 
-#ifdef BLZ3_USE_SSE2
 		result.v = _mm_mul_pd(v, _mm_set1_pd(value));
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			result.v[i] = v[i] * value;
-		}
-#endif
 		return result;
 	}
 
 	inline void operator*=(const b3_f64 value)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = _mm_mul_pd(v, _mm_set1_pd(value));
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			v[i] *= value;
-		}
-#endif
 	}
 
 	inline b3Complex64 operator/(const b3_f64 value) const
 	{
 		b3Complex64 result;
 
-#ifdef BLZ3_USE_SSE2
 		result.v = _mm_div_pd(v, _mm_set1_pd(value));
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			result.v[i] = v[i] / value;
-		}
-#endif
 		return result;
 	}
 
 	inline void operator/=(const b3_f64 value)
 	{
-#ifdef BLZ3_USE_SSE2
 		v = _mm_div_pd(v, _mm_set1_pd(value));
-#else
-		for (int i = 0; i < 2; i++)
-		{
-			v[i] /= value;
-		}
-#endif
 	}
 
 	inline b3_f64 b3SquareLength() const
 	{
-#ifdef BLZ3_USE_SSE2
 		alignas(16) b3_f64 comp[2];
 
 		_mm_store_pd(comp, _mm_mul_pd(v, v));
 
 		return comp[0] + comp[1];
-#else
-		return v[0] * v[0] + v[1] * v[1];
-#endif
 	}
 
 	inline b3_f64 b3Length() const
@@ -285,28 +182,20 @@ public:
 
 	inline b3_f64 & b3Real() const
 	{
-#ifdef BLZ3_USE_SSE2
 		return (b3_f64 &)v;
-#else
-		return v[0];
-#endif
 	}
 
 	inline b3_f64 & b3Imag() const
 	{
-#ifdef BLZ3_USE_SSE2
 		b3_f64 * ptr = (b3_f64 *)&v;
 
 		return ptr[1];
-#else
-		return v[1];
-#endif
 	}
 
 	inline static b3Complex64 b3Sqrt(const b3Complex64 & a)
 	{
 		_MM_SET_EXCEPTION_STATE(0);
-		__m128d s = _mm_sqrt_pd(SSE_PD_LOAD(a.v));
+		__m128d s = _mm_sqrt_pd(a.v);
 		unsigned int csr = _mm_getcsr();
 
 		if (csr & _MM_EXCEPT_INVALID)
@@ -324,20 +213,9 @@ public:
 
 	inline static void b3Swap(b3Complex64 & a, b3Complex64 & b)
 	{
-#ifdef BLZ3_USE_SSE2
 		__m128d aux = a.v;
 		a.v = b.v;
 		b.v = aux;
-#else
-		b3_f64 aux;
-
-		aux = a.v[0];
-		a.v[0] = b.v[0];
-		b.v[0] = aux;
-		aux = a.v[1];
-		a.v[1] = b.v[1];
-		b.v[1] = aux;
-#endif
 	}
 };
 
