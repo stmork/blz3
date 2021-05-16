ec0d470edbbc793d8b635d2042e6d767fef62eb6
Steffen A. Mork <linux-dev@morknet.de>
2021-05-16 13:50:23 +0200

    Moved NURBS tests into dedicated file.

--- src/unittest/b3AuxTest.h
+++ src/unittest/b3AuxTest.h
@@ -17,8 +17,8 @@
 
 #pragma once
 
-#ifndef B3_SKELETON_TEST_H
-#define B3_SKELETON_TEST_H
+#ifndef B3_AUX_TEST_H
+#define B3_AUX_TEST_H
 
 #include "blz3/b3Config.h"
 
@@ -26,8 +26,6 @@
 
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
-
-#include "blz3/base/b3Spline.h"
 
 class b3AuxTest : public CppUnit::TestFixture
 {
@@ -49,34 +47,6 @@ public:
 	void testPoint();
 	void testStrCase();
 	void testStrCaseCmp();
-};
-
-class b3NurbsSurfaceTest : public CppUnit::TestFixture
-{
-	static constexpr double RADIUS = 10.0;
-
-	b3Nurbs           m_Horizontal;
-	b3Nurbs           m_Vertical;
-	b3Spline::b3_knot m_HorizontalKnots[b3Nurbs::B3_MAX_KNOTS];
-	b3Spline::b3_knot m_VerticalKnots[b3Nurbs::B3_MAX_KNOTS];
-	b3_vector4D       m_Controls[b3Nurbs::B3_MAX_CONTROLS * b3Nurbs::B3_MAX_CONTROLS];
-
-	b3_vector4D       m_Deboor[b3Nurbs::B3_MAX_SUBDIV];
-	b3_vector4D       m_Mansfield[b3Nurbs::B3_MAX_SUBDIV];
-	b3_f64            m_Radius[b3Nurbs::B3_MAX_SUBDIV + 1];
-	b3_f64            m_BasisCoeff[b3Nurbs::B3_MAX_DEGREE];
-
-	CPPUNIT_TEST_SUITE(b3NurbsSurfaceTest);
-	CPPUNIT_TEST(testSphereHorizontally);
-	CPPUNIT_TEST(testSphereVertically);
-	CPPUNIT_TEST_SUITE_END();
-
-public:
-	void setUp() override;
-	void tearDown() override;
-
-	void testSphereHorizontally();
-	void testSphereVertically();
 };
 
 #endif
--- src/unittest/b3NurbsTest.cc
+++ src/unittest/b3NurbsTest.cc
@@ -34,6 +34,7 @@
 
 CPPUNIT_TEST_SUITE_REGISTRATION(b3NurbsClosedCurveTest);
 CPPUNIT_TEST_SUITE_REGISTRATION(b3NurbsOpenedCurveTest);
+CPPUNIT_TEST_SUITE_REGISTRATION(b3NurbsSurfaceTest);
 
 /*************************************************************************
 **                                                                      **
@@ -225,6 +226,185 @@ void b3NurbsOpenedCurveTest::testCircle()
 		CPPUNIT_ASSERT_LESSEQUAL(    m_Nurbs.b3LastKnot(),  b3Nurbs::b3_knot(q));
 	}
 	b3PrintF(B3LOG_DEBUG, "%p\n", m_Radius);
+}
+
+/*************************************************************************
+**                                                                      **
+**                        Unit test for NURBS surfaces                  **
+**                                                                      **
+*************************************************************************/
+
+void b3NurbsSurfaceTest::setUp()
+{
+	m_Horizontal.m_Knots    = m_HorizontalKnots;
+	m_Horizontal.m_Controls = m_Controls;
+	m_Horizontal.b3InitCurve(2, 8, true);
+
+	m_Vertical.m_Knots      = m_VerticalKnots;
+	m_Vertical.m_Controls   = m_Controls;
+	m_Vertical.b3InitCurve(2, 5, false);
+	m_Vertical.m_Offset     = m_Horizontal.m_ControlMax;
+
+	for (unsigned i = 0; i < m_Horizontal.m_KnotNum; i++)
+	{
+		m_HorizontalKnots[i] = std::ceil(i * 0.5);
+	}
+	for (unsigned i = 0; i < m_Vertical.m_KnotNum; i++)
+	{
+		m_VerticalKnots[i] = std::max(0.0, std::ceil(i * 0.5 - 1.0));
+	}
+
+	for (unsigned y = 0; y < m_Vertical.m_ControlNum; y++)
+	{
+		const double y_angle = y * M_PI / (m_Vertical.m_ControlNum - 1);
+		const double height = sin(y_angle);
+		unsigned i = y * m_Horizontal.m_ControlMax;
+
+		for (unsigned x = 0; x < m_Horizontal.m_ControlNum; x++)
+		{
+			const double   x_angle = x * 2.0 * M_PI / m_Horizontal.m_ControlNum;
+			const unsigned level   = (x & 1) + (y & 1);
+
+			m_Controls[i].x =  cos(x_angle) * RADIUS * height;
+			m_Controls[i].y =  sin(x_angle) * RADIUS * height;
+			m_Controls[i].z = -cos(y_angle) * RADIUS;
+
+			switch (level)
+			{
+			case 0:
+				m_Controls[i].w = 1;
+				break;
+			case 1:
+				m_Controls[i].w = sqrt(2.0) * 0.5;
+				break;
+			case 2:
+				m_Controls[i].w = 0.5;
+				break;
+
+			default:
+				// Intentionally do nothing
+				break;
+			}
+
+			i++;
+		}
+	}
+
+	bzero(m_Deboor,     sizeof(m_Deboor));
+	bzero(m_Mansfield,  sizeof(m_Mansfield));
+	bzero(m_Radius,     sizeof(m_Radius));
+	bzero(m_BasisCoeff, sizeof(m_BasisCoeff));
+
+	b3PrintF(B3LOG_DEBUG, "Setup: %s\n", __FILE__);
+}
+
+void b3NurbsSurfaceTest::tearDown()
+{
+	b3PrintF(B3LOG_DEBUG, "Tear down: %s\n", __FILE__);
+}
+
+void b3NurbsSurfaceTest::testSphereHorizontally()
+{
+	const b3_f64   x_range = m_Horizontal.b3KnotRange();
+	const b3_f64   y_range = m_Vertical.b3KnotRange();
+
+	CPPUNIT_ASSERT_EQUAL(4.0, x_range);
+	CPPUNIT_ASSERT_EQUAL(2.0, y_range);
+
+	b3_vector4D aux_control_points[b3Nurbs::B3_MAX_CONTROLS * b3Nurbs::B3_MAX_CONTROLS];
+	b3Nurbs     aux_nurbs;
+
+	// building horizontal splines
+	// first create controls for segments of vertical spline...
+	b3Nurbs::b3DeBoorSurfaceControl(&m_Horizontal, &m_Vertical, aux_control_points);
+	aux_nurbs            = m_Horizontal;
+	aux_nurbs.m_Offset   = m_Vertical.b3GetSegmentKnotCount();
+	aux_nurbs.m_Controls = aux_control_points;
+
+	for (b3_index y = 0; y < aux_nurbs.m_Offset; y++)
+	{
+		const unsigned count = aux_nurbs.b3DeBoor(m_Deboor, y);
+		CPPUNIT_ASSERT_EQUAL(aux_nurbs.m_SubDiv + 1, count);
+
+		const b3_f64 range = aux_nurbs.b3KnotRange();
+
+		for (unsigned s = 0; s <= aux_nurbs.m_SubDiv; s++)
+		{
+			const b3_f64 q   = aux_nurbs.b3FirstKnot() + s * range / aux_nurbs.m_SubDiv;
+			const unsigned i = aux_nurbs.b3Mansfield(m_BasisCoeff, q);
+
+			aux_nurbs.b3MansfieldVector(&m_Mansfield[s], m_BasisCoeff, i, y);
+
+			// Compare De Boor computed values agains Mansfield computed ones
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].x, m_Mansfield[s].x, b3Nurbs::epsilon);
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].y, m_Mansfield[s].y, b3Nurbs::epsilon);
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].z, m_Mansfield[s].z, b3Nurbs::epsilon);
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].w, m_Mansfield[s].w, b3Nurbs::epsilon);
+
+			// Validate radius agains circle.
+			m_Radius[s] = sqrt(
+					m_Mansfield[s].x * m_Mansfield[s].x +
+					m_Mansfield[s].y * m_Mansfield[s].y +
+					m_Mansfield[s].z * m_Mansfield[s].z);
+#if 0
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(RADIUS, m_Radius[s], b3Nurbs::epsilon);
+#else
+			b3PrintF(B3LOG_FULL, "Radius: %p\n", m_Radius);
+#endif
+		}
+	}
+}
+
+void b3NurbsSurfaceTest::testSphereVertically()
+{
+	const b3_f64   x_range = m_Horizontal.b3KnotRange();
+	const b3_f64   y_range = m_Vertical.b3KnotRange();
+
+	CPPUNIT_ASSERT_EQUAL(4.0, x_range);
+	CPPUNIT_ASSERT_EQUAL(2.0, y_range);
+
+	b3_vector4D aux_control_points[b3Nurbs::B3_MAX_CONTROLS * b3Nurbs::B3_MAX_CONTROLS];
+	b3Nurbs     aux_nurbs;
+
+	// building horizontal splines
+	// first create controls for segments of vertical spline...
+	b3Nurbs::b3DeBoorSurfaceControl(&m_Vertical, &m_Horizontal, aux_control_points);
+	aux_nurbs            = m_Vertical;
+	aux_nurbs.m_Offset   = m_Horizontal.b3GetSegmentKnotCount();
+	aux_nurbs.m_Controls = aux_control_points;
+
+	for (b3_index y = 0; y < aux_nurbs.m_Offset; y++)
+	{
+		const unsigned count = aux_nurbs.b3DeBoor(m_Deboor, y);
+		CPPUNIT_ASSERT_EQUAL(aux_nurbs.m_SubDiv + 1, count);
+
+		const b3_f64 range = aux_nurbs.b3KnotRange();
+
+		for (unsigned s = 0; s <= aux_nurbs.m_SubDiv; s++)
+		{
+			const b3_f64 q   = aux_nurbs.b3FirstKnot() + s * range / aux_nurbs.m_SubDiv;
+			const unsigned i = aux_nurbs.b3Mansfield(m_BasisCoeff, q);
+
+			aux_nurbs.b3MansfieldVector(&m_Mansfield[s], m_BasisCoeff, i, y);
+
+			// Compare De Boor computed values agains Mansfield computed ones
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].x, m_Mansfield[s].x, b3Nurbs::epsilon);
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].y, m_Mansfield[s].y, b3Nurbs::epsilon);
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].z, m_Mansfield[s].z, b3Nurbs::epsilon);
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(m_Deboor[s].w, m_Mansfield[s].w, b3Nurbs::epsilon);
+
+			// Validate radius agains circle.
+			m_Radius[s] = sqrt(
+					m_Mansfield[s].x * m_Mansfield[s].x +
+					m_Mansfield[s].y * m_Mansfield[s].y +
+					m_Mansfield[s].z * m_Mansfield[s].z);
+#if 0
+			CPPUNIT_ASSERT_DOUBLES_EQUAL(RADIUS, m_Radius[s], b3Nurbs::epsilon);
+#else
+			b3PrintF(B3LOG_FULL, "Radius: %p\n", m_Radius);
+#endif
+		}
+	}
 }
 
 #endif
